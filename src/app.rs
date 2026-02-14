9c83d8285dcce95fbc5c859939edfac7a1736b27
jgordini <jgordini@uab.edu>
2026-02-14 10:57:06 -0600

    Update favicon and add cache-busting query param

--- src/app.rs
+++ src/app.rs
@@ -15,7 +15,7 @@ pub fn shell(options: LeptosOptions) -> impl IntoView {
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
                 <AutoReload options=options.clone() />
                 <HydrationScripts options/>
-                <link rel="shortcut icon" type="image/ico" href="/favicon.ico"/>
+                <link rel="shortcut icon" type="image/ico" href="/favicon.ico?v=20260214"/>
                 <MetaTags/>
             </head>
             <body>
